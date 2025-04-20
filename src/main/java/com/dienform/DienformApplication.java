f8b9700cf302fbbde5694495c8b132bc26bfd878
tuanlva20 <tuanlva20@gmail.com>
2025-04-20 17:53:52 +0700

    first commit

---
+++ src/main/java/com/dienform/DienformApplication.java
@@ -0,0 +1,13 @@
+package com.dienform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DienformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DienformApplication.class, args);
+	}
+
+}
